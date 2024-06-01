f6b7e916d3bb676809849864f4343e28a85c3bb7
aroachans <aroa.chans@gmail.com>
2024-06-01 17:02:36 +0200

    coches

--- your-code/create.sql
+++ your-code/create.sql
@@ -1,78 +1,38 @@
-DROP DATABASE IF EXISTS cars;
-CREATE DATABASE cars; 
-USE cars;
+USE lab_mysql;
+CREATE TABLE Coches (
+    VIN VARCHAR(20) not null Primary key,
+    Fabricante VARCHAR(20),
+    Modelo VARCHAR(20),
+    Año INT,
+    Color VARCHAR(20)
+);
 
--- -----------------------------------------------------
--- Table `cars`.`cars`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cars`.`cars` (
-  `car_id` INT NOT NULL,
-  `manufacturer` VARCHAR(20) NOT NULL,
-  `model` VARCHAR(20) NOT NULL,
-  `year_` INT NOT NULL,
-  `color` VARCHAR(20) NOT NULL,
-  PRIMARY KEY (`car_id`));
+CREATE TABLE Vendedores (
+    vendedor_id INT not null Primary key,
+    Nombre VARCHAR(20),
+    Tienda VARCHAR(5)
+);
 
+CREATE TABLE Clientes (
+    Client_id int not null AUTO_INCREMENT Primary key,
+    Nombre VARCHAR(20),
+    Apellido VARCHAR(20),
+    Tlf INT, -- Asumiendo que el teléfono es un número entero
+    Email VARCHAR(30),
+    Direccion VARCHAR(20),
+    Ciudad VARCHAR(20),
+    Provincia VARCHAR(20),
+    Pais VARCHAR(200),
+    CodigoPostal int
+);
 
--- -----------------------------------------------------
--- Table `cars`.`costumer`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cars`.`costumer` (
-  `costumer_id` INT NOT NULL,
-  `first_name` VARCHAR(20) NOT NULL,
-  `last_name` VARCHAR(20),
-  `phone_no` VARCHAR(20) NULL DEFAULT NULL,
-  `email` VARCHAR(20) NULL DEFAULT NULL,
-  `address` VARCHAR(40) NULL DEFAULT NULL,
-  `city` VARCHAR(20) NULL DEFAULT NULL,
-  `state` VARCHAR(20) NULL DEFAULT NULL,
-  `country` VARCHAR(20) NULL DEFAULT NULL,
-  `zip_code` VARCHAR(20) NULL DEFAULT NULL,
-  `client` INT NULL DEFAULT NULL,
-  PRIMARY KEY (`costumer_id`));
-
-
-  
-  -- -----------------------------------------------------
--- Table `cars`.`salesperson`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cars`.`salesperson` (
-  `salesperson_id` INT NOT NULL,
-  `first_name` VARCHAR(40) NOT NULL,
-  `store_name` VARCHAR(3) NOT NULL,
-  PRIMARY KEY (`salesperson_id`));
-
-- -----------------------------------------------------
--- Table `cars`.`invoices`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cars`.`invoices` (
-  `invoice_id` INT NOT NULL,
-  `date_` DATE NULL DEFAULT NULL,
-  `car` VARCHAR(20) NOT NULL,
-  `costumer` VARCHAR(20) NULL DEFAULT NULL,
-  `salesperson` VARCHAR(20) NULL DEFAULT NULL,
-  `salesperson_salesperson_id` INT NOT NULL,
-  `cars_car_id` INT NOT NULL,
-  `costumer_costumer_id` INT NOT NULL,
-  PRIMARY KEY (`invoice_id`),
-  INDEX `fk_invoices_salesperson_idx` (`salesperson_salesperson_id` ASC) VISIBLE,
-  INDEX `fk_invoices_cars1_idx` (`cars_car_id` ASC) VISIBLE,
-  INDEX `fk_invoices_costumer1_idx` (`costumer_costumer_id` ASC) VISIBLE,
-  CONSTRAINT `fk_invoices_salesperson`
-    FOREIGN KEY (`salesperson_salesperson_id`)
-    REFERENCES `cars`.`salesperson` (`salesperson_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_invoices_cars1`
-    FOREIGN KEY (`cars_car_id`)
-    REFERENCES `cars`.`cars` (`car_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_invoices_costumer1`
-    FOREIGN KEY (`costumer_costumer_id`)
-    REFERENCES `cars`.`costumer` (`costumer_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-
-
-
+CREATE TABLE Facturas (
+    Factura_id INT not null AUTO_INCREMENT Primary key,
+    Fecha timestamp,
+    Coche VARCHAR(20),
+    Cliente INT,
+    Vendedor INT, -- Cambiado de "int" a "INT"
+    foreign key(Coches) references Coches(VIN),
+    foreign key(Clientes) references Clientes(Client_id),
+    foreign key(Vendedores) references Vendedores(vendedor_id)
+);
